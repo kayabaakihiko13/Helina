d5a1f9f54ff90f490b65286a964a961a15e6ad76
slowy07 <slowy.arfy@gmail.com>
2023-08-13 14:18:41 +0700

    fix: try to fixing unitesting
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- hela_testing/main_test.py
+++ hela_testing/main_test.py
@@ -1,6 +1,6 @@
 import unittest
-from hela_testing.math_testing import MathTest
-from hela_testing.euler_test import EulerTotientTest
+from math_testing import MathTest
+from euler_test import EulerTotientTest
 
 if __name__ == "__main__":
     MathTest
