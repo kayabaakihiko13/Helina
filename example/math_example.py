ca85729e1939c3ec7e0ad2eb18ae5d7568d7c147
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-08-16 17:52:13 +0700

    feat: adding algotihm cosine and exponential (#1)
    * feat: adding algotihm cosine and exponential
    
    Signed-off-by: KayabaAkihiko13 <iqbalramad75@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: KayabaAkihiko13 <iqbalramad75@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- example/math_example.py
+++ example/math_example.py
@@ -21,5 +21,17 @@ def mean_of_list_numbers() -> None:
     print(f"average mean of {avg_mean_list} is: {hela.avg_mean(avg_mean_list)}")
 
 
+def finding_cosine() -> None:
+    """
+    finding cosine
+    """
+    hela = Math()
+    angle_in_radians: int = 0
+    print(
+        f"value cosine in {angle_in_radians} angle is {hela.cosine(angle_in_radians)}"
+    )
+
+
 finding_radians()
 mean_of_list_numbers()
+finding_cosine()
