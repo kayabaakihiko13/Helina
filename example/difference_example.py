3984a1b47c82f730408c1085e7bf2991e8b3c7f7
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-09-08 22:06:53 +0700

    feat: add example for differential and fixing (#8)
    * feat: add example for differential and fixing
    
    [Decription]
    what is add example in differential-example.py
    
    - Defivative
    - Power Derivative
    - Product Derivate
    - Derivative Quotient
    
    fixing
    
    just result of doctest in product Derivative and the declaration data type
    
    Signed-off-by: kayabaakihiko13 <iqbalramad75@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: kayabaakihiko13 <iqbalramad75@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ example/difference_example.py
@@ -0,0 +1,30 @@
+from Hela.common.differential import Differential
+
+if __name__ == "__main__":
+    # this for Defivative example
+    print("Differential Derivative")
+    func = lambda x: x**2
+    value_input: int = 3
+    calculate_defivative: float = Differential.derivative(func, value_input)
+    print(f"result Derivative:{calculate_defivative:.3f}")
+
+    # Diffetential Power Derivative
+    print("\nPower Derivative")
+    exponent_input: int = 6
+    calculate_power_derivative = Differential.power_derivative(exponent_input, 2)
+
+    print(f"result Power Derivative:{calculate_power_derivative:.3f}")
+
+    # Differential Product Derivative
+    print("\nProduct Derivative")
+    a_func = lambda x: x**2
+    b_func = lambda x: x
+    input_value: int = 3
+    result = Differential.product_derivate(a_func, b_func, 2)
+    print(f"Result Product Derivate {result}")
+    # Differential Derivative Quotient
+    print("\nDerivative Quotient")
+    u_func: function = lambda x: x**2
+    v_func: function = lambda x: x
+    result = Differential.quotient_derivate(u_func, v_func, 2)
+    print(f"Result of Derivative Quotient:{result}")
