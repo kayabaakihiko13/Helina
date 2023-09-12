fb8e6e14dcbc1c0c67cf78866bfd5f293d22b6e1
kayabaakihiko13 <iqbalramad75@gmail.com>
2023-09-13 04:47:37 +0700

    fix: repair function
    [Decription]
    the repair is about value differential in product derivate and quotient derivate
    
    - Product Derivate
    - Quotient Derivate
    
    Signed-off-by: kayabaakihiko13 <iqbalramad75@gmail.com>

--- Hela/common/differential.py
+++ Hela/common/differential.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import math
 
 
@@ -10,7 +11,7 @@ class Differential(object):
     """
 
     @staticmethod
-    def derivative(f, x: float, h: float = 1e-6) -> float:
+    def derivative(f, x: float|int, h: float = 1e-6) -> float:
         """
         calculate the derivative of a function using the limit definition
 
@@ -28,15 +29,15 @@ class Differential(object):
         >>> diff = Differential()
         >>> result = diff.derivative(f, 2)
         >>> print(result)
-        4.00000016666679
+        4.0000
         """
         try:
-            return (f(x + h) - f(x)) / h
+            return round((f(x + h) - f(x)) / h, 4)
         except Exception as error_derivative:
             raise ValueError(f"error {error_derivative}")
 
     @staticmethod
-    def power_derivative(n: int, x: float) -> float:
+    def power_derivative(n: int, x: float|int) -> float:
         """
         calculate the derivative of x^n using the power rule
 
@@ -51,7 +52,7 @@ class Differential(object):
         >>> diff = Differential()
         >>> result = diff.power_derivative(3, 2)
         >>> print(result)
-        12
+        12.0
         """
         try:
             return n * math.pow(x, n - 1)
@@ -59,7 +60,8 @@ class Differential(object):
             raise ValueError(f"error: {error_power_derivative}")
 
     @staticmethod
-    def product_derivate(u, v, x: float) -> float:
+    def product_derivate(u, v,
+                         x: float | int) -> float:
         """
         calculate the derivative of the product of two functions u(x) and v(x)
         using product rule
@@ -67,7 +69,7 @@ class Differential(object):
         Args:
             u (function): first function u(x)
             v (function): second function v(x)
-            x (float): the value at which to calculate the derivative
+            x (float,int): the value at which to calculate the derivative
 
         Returns:
             (float): the calculated derivative
@@ -80,17 +82,18 @@ class Differential(object):
         >>> diff = Differential()
         >>> result = diff.product_derivate(f, g, 2)
         >>> print(result)
-        64
+        80.0
         """
         try:
-            u_derivative = u(x)
-            v_derivative = v(x)
+            u_derivative = Differential.derivative(u,x)
+            v_derivative = Differential.derivative(v,x)
             return u_derivative * v(x) + u(x) * v_derivative
         except Exception as error_product_derivative:
             raise ValueError(f"error {error_product_derivative}")
 
     @staticmethod
-    def quotient_derivate(u, v, x):
+    def quotient_derivate(u, v,
+                          x: float | int):
         """
         calculate the derivative of the quotient of two functions u(x) and v(x) using
         quotient rule
@@ -111,17 +114,18 @@ class Differential(object):
         >>> diff = Differential()
         >>> result = diff.quotient_derivate(f, h, 2)
         >>> print(result)
-        1.0
+        -1.0
         """
         try:
-            u_derivative = u(x)
-            v_derivative = v(x)
+            u_derivative = Differential.derivative(u,x)
+            v_derivative = Differential.derivative(v,x)
             return (v_derivative * u(x) - u_derivative * v(x)) / math.pow(v(x), 2)
         except Exception as error_quotient_derivative:
             raise ValueError(f"error: {error_quotient_derivative}")
 
     @staticmethod
-    def composite_derivative(w, u, x: int) -> float:
+    def composite_derivative(w, u,
+                             x: int | float) -> float:
         """
         calculate the derivative of the composite function w(u(x)) using chain rule
 
@@ -141,11 +145,12 @@ class Differential(object):
         >>> diff = Differential()
         >>> result = diff.composite_derivative(g, k, 2)
         >>> print(result)
-        625
+        300
         """
         try:
-            w_derivative = w(u(x))
-            u_derivative = u(x)
+            
+            w_derivative = Differential.derivative(w,u(x))
+            u_derivative = Differential.derivative(u,x)
             return w_derivative * u_derivative
         except Exception as error_composite_derivative:
             raise ValueError(f"error: {error_composite_derivative}")
